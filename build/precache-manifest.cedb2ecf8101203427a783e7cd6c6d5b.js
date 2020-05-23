2d8ccd6ee649f7bc1a8dee895ff90140c7d48287
Martins <34019878+martinszeltins@users.noreply.github.com>
2020-05-23 08:28:08 +0300

    Add files via upload

---
+++ build/precache-manifest.cedb2ecf8101203427a783e7cd6c6d5b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a451457bc7d4dd37df251002161f141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5aca83113088d3df630",
+    "url": "/static/css/main.2a5d1a5e.chunk.css"
+  },
+  {
+    "revision": "008f048529f4b8d51397",
+    "url": "/static/js/2.38c2f2fe.chunk.js"
+  },
+  {
+    "revision": "e5aca83113088d3df630",
+    "url": "/static/js/main.f6d6bff8.chunk.js"
+  },
+  {
+    "revision": "5226aef5af858629dc69",
+    "url": "/static/js/runtime-main.a6078d24.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
